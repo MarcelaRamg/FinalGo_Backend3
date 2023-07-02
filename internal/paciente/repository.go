31c8128df742dd50655fa46260c91d8fba829155
Agus Rivarola <agurivarola@gmail.com>
2023-07-02 00:10:52 -0300

    implemento el repositorio y el servicio de paciente

--- internal/paciente/repository.go
+++ internal/paciente/repository.go
@@ -1,1 +1,57 @@
 package paciente
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/MarcelaRamg/FinalBack3.git/internal/domain"
+	"github.com/MarcelaRamg/FinalBack3.git/pkg/pacientePkg"
+)
+
+type Repository interface {
+	GetByID(id int) (domain.Paciente, error)
+	Create(p domain.Paciente) (domain.Paciente, error)
+	Update(id int, p domain.Paciente) (domain.Paciente, error)
+	Delete(id int) error
+}
+
+type repository struct {
+	storage pacientePkg.PacienteInterface
+}
+
+func NewRepository(storage pacientePkg.PacienteInterface) Repository {
+	return &repository{storage}
+}
+
+func (r *repository) GetByID(id int) (domain.Paciente, error) {
+	paciente, err := r.storage.Read(id)
+	if err != nil {
+		return domain.Paciente{}, errors.New("Paciente not found")
+	}
+	return paciente, nil
+
+}
+
+func (r *repository) Create(p domain.Paciente) (domain.Paciente, error) {
+	err := r.storage.Create(p)
+	if err != nil {
+		return domain.Paciente{}, errors.New(fmt.Sprintf("error creating Paciente: %s", err.Error()))
+	}
+	return p, nil
+}
+
+func (r *repository) Delete(id int) error {
+	err := r.storage.Delete(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *repository) Update(id int, p domain.Paciente) (domain.Paciente, error) {
+	err := r.storage.Update(p)
+	if err != nil {
+		return domain.Paciente{}, errors.New("error .. updating Paciente")
+	}
+	return p, nil
+}
--- internal/paciente/service.go
+++ internal/paciente/service.go
@@ -1,1 +1,72 @@
 package paciente
+
+import (
+	"github.com/MarcelaRamg/FinalBack3.git/internal/domain"
+)
+
+type Service interface {
+	// GetByID busca un producto por su id
+	GetByID(id int) (domain.Paciente, error)
+	// Create agrega un nuevo producto
+	Create(p domain.Paciente) (domain.Paciente, error)
+	// Delete elimina un producto
+	Delete(id int) error
+	// Update actualiza un producto
+	Update(id int, p domain.Paciente) (domain.Paciente, error)
+}
+
+type service struct {
+	r Repository
+}
+
+// NewService crea un nuevo servicio
+func NewService(r Repository) Service {
+	return &service{r}
+}
+
+func (s *service) GetByID(id int) (domain.Paciente, error) {
+	p, err := s.r.GetByID(id)
+	if err != nil {
+		return domain.Paciente{}, err
+	}
+	return p, nil
+}
+
+func (s *service) Create(p domain.Paciente) (domain.Paciente, error) {
+	p, err := s.r.Create(p)
+	if err != nil {
+		return domain.Paciente{}, err
+	}
+	return p, nil
+}
+func (s *service) Update(id int, u domain.Paciente) (domain.Paciente, error) {
+	p, err := s.r.GetByID(id)
+	if err != nil {
+		return domain.Paciente{}, err
+	}
+	if u.Name == "" {
+		p.Name = u.Name
+	}
+	if u.Apellido == "" {
+		p.Apellido = u.Apellido
+	}
+	if u.Dni == "" {
+		p.Dni = u.Dni
+	}
+	if u.FechaNacimiento == "" {
+		p.FechaNacimiento = u.FechaNacimiento
+	}
+	p, err = s.r.Update(id, p)
+	if err != nil {
+		return domain.Paciente{}, err
+	}
+	return p, nil
+}
+
+func (s *service) Delete(id int) error {
+	err := s.r.Delete(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
